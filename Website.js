96ac6a08113bf6a0824e5401816b2bb46bcae32e
TredonA <Tausti@umich.edu>
2018-12-27 09:39:33 -0500

    Minor introduction changes as well as new javascript file

---
+++ Website.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+function scrollToPortfolio() {
+    var portfolio = document.getElementById("SamplePortfolios");
+    portfolio.scrollIntoView();
+    behavior: 'smooth';
+}
